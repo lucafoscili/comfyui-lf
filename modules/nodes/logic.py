53b0b7c0098ff52e02ca846a0938f26893594e3c
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-09-20 17:58:59 +0200

    feat: new node Extractor

--- modules/nodes/logic.py
+++ modules/nodes/logic.py
@@ -1,6 +1,64 @@
+import json
+
 from server import PromptServer
 
 category = "✨ LF Nodes/Logic"
+
+class LF_Extractor:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "text": ("STRING", {"default": "", "multiline": True, "tooltip": "The string from which the output will be extracted."}),
+                "starting_delimiter": ("STRING", {"default": "{", "tooltip": "The delimiter where extraction starts."}),
+                "ending_delimiter": ("STRING", {"default": "}", "tooltip": "The delimiter where extraction ends."}),
+            },
+            "hidden": { "node_id": "UNIQUE_ID" },
+        }
+
+    CATEGORY = category
+    FUNCTION = "on_exec"
+    RETURN_NAMES = ("as_json", "as_text", "as_int", "as_float", "as_boolean")
+    RETURN_TYPES = ("JSON", "STRING", "INT", "FLOAT", "BOOLEAN")
+
+    def on_exec(self, node_id, text: str, starting_delimiter: str, ending_delimiter: str):
+        try:
+            start_idx = text.index(starting_delimiter)
+            end_idx = text.index(ending_delimiter, start_idx) + len(ending_delimiter)
+            extracted_text = text[start_idx:end_idx].strip()
+        except ValueError:
+            extracted_text = ""
+
+        result_as_text = extracted_text
+        result_as_json = None
+        result_as_int = None
+        result_as_float = None
+        result_as_boolean = None
+
+        try:
+            result_as_json = json.loads(result_as_text)
+        except (ValueError, json.JSONDecodeError):
+            pass
+
+        try:
+            result_as_int = int(result_as_text)
+        except ValueError:
+            pass
+
+        try:
+            result_as_float = float(result_as_text)
+        except ValueError:
+            pass
+
+        result_as_boolean = result_as_text.lower() in ("true", "yes", "1")
+
+        PromptServer.instance.send_sync("lf-extractor", {
+            "node": node_id, 
+            "result": result_as_text,
+        })
+
+        return (result_as_json, result_as_text, result_as_int, result_as_float, result_as_boolean)
+
 
 class LF_SwitchFloat:
     @classmethod
@@ -163,6 +221,7 @@ class LF_SwitchString:
         return (on_true if boolean else on_false,)
     
 NODE_CLASS_MAPPINGS = {
+    "LF_Extractor": LF_Extractor,
     "LF_SwitchFloat": LF_SwitchFloat,
     "LF_SwitchImage": LF_SwitchImage,
     "LF_SwitchInteger": LF_SwitchInteger,
@@ -170,6 +229,7 @@ NODE_CLASS_MAPPINGS = {
     "LF_SwitchString": LF_SwitchString,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "LF_Extractor": "Extracts something from text",
     "LF_SwitchFloat": "Switch Float",
     "LF_SwitchImage": "Switch Image",
     "LF_SwitchInteger": "Switch Integer",
