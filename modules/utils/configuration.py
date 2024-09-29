048971ba773d3c340c6e1a89f9ab5c23918025ca
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-09-29 21:32:05 +0200

    refactor: moved model dataset preparation to utility function

--- modules/utils/configuration.py
+++ modules/utils/configuration.py
@@ -92,7 +92,7 @@ def get_lora_hashes(lora_tags):
     return lora_hashes
 
 def get_sha256(file_path):
-    hash_file_path = f"{file_path}.sha256"
+    hash_file_path = f"{os.path.splitext(file_path)[0]}.sha256"
     
     if os.path.exists(hash_file_path):
         with open(hash_file_path, "r") as hash_file:
@@ -109,17 +109,54 @@ def get_sha256(file_path):
 
     return sha256_value.hexdigest()[:10]
 
+def prepare_model_dataset (model_name, model_hash, model_base64, model_path):
+    dataset = {
+                "nodes": [
+                    {
+                        "cells": {
+                            "icon": {
+                                "kulStyle": "img {object-fit: cover;}",
+                                "shape": "image",
+                                "value": "data:image/webp;base64," + model_base64 if model_path else "broken_image"
+                            },
+                            "text1": {
+                                "value": model_name
+                            },
+                            "text2": {
+                                "value": model_hash
+                            },
+                            "text3": {
+                                "value": "Selected checkpoint cover, hash and name." +
+                                         ("" if model_path 
+                                             else "Note: to set the cover, create an image with the same name of the checkpoint in its folder.")
+                            }
+                        },
+                        "id": model_name
+                    }
+                ]
+            }
+    
+    return dataset
+
 def process_model(model_type, model_name, folder):
     model_path = folder_paths.get_full_path(folder, model_name)
     model_info_path = os.path.splitext(model_path)[0] + ".info"
 
     saved_info = None
     if os.path.exists(model_info_path):
-        with open(model_info_path, 'r') as f:
-            saved_info = json.load(f)
-        print(f"Found existing {model_type} info file at {model_info_path}.")
-    else:
-        print(f"No {model_type} info file found at {model_info_path}. Using CivitAI if needed.")
+        try:
+            with open(model_info_path, 'r') as f:
+                file_content = f.read().strip()
+
+                if not file_content:
+                    os.remove(model_info_path)
+                else:
+                    try:
+                        saved_info = json.loads(file_content)
+                    except json.JSONDecodeError as e:
+                        print(f"JSONDecodeError for {model_info_path}: {e}")
+        except Exception as e:
+            print(f"Error reading {model_info_path}: {e}")
 
     try:
         model_hash = get_sha256(model_path)
@@ -127,15 +164,13 @@ def process_model(model_type, model_name, folder):
         model_hash = "Unknown"
         print(f"Error calculating hash for {model_type}: {e}")
 
-    model_image_path = find_checkpoint_image(model_path) if model_type == "checkpoint" else None
+    model_image_path = find_checkpoint_image(model_path)
 
     if model_image_path:
-        print(f"Found image for {model_type}: {model_image_path}")
         pil_image = Image.open(model_image_path)
         model_cover = pil_to_tensor(pil_image)
         model_base64 = tensor_to_base64(model_cover)
     else:
-        print(f"No image found for {model_type}.")
         model_base64 = "None"
         model_cover = None
 
