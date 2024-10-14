42f450c50df47ce06a7c181db18d93ccead9a5a1
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-10-14 22:23:11 +0200

    feat (card): handled tooltip on right click to upload new covers for selectors

--- web/deploy/js/helpers/card.js
+++ web/deploy/js/helpers/card.js
@@ -1,5 +1,5 @@
 import { LogSeverity } from '../types/manager.js';
-import { getLFManager, deserializeValue } from '../utils/common.js';
+import { getLFManager, deserializeValue, getApiRoutes } from '../utils/common.js';
 export const CARD_PROPS_TO_SERIALIZE = ['kulData', 'kulStyle'];
 export const cardHandler = (container, propsArray) => {
     let count = 0;
@@ -24,7 +24,9 @@ export const cardHandler = (container, propsArray) => {
                             const node = card.kulData.nodes?.[0];
                             if (node) {
                                 card.dataset.link = node.description;
-                                card.title = String(node.value).valueOf();
+                                if (node.value) {
+                                    card.title = String(node.value).valueOf();
+                                }
                             }
                         }
                         catch (error) {
@@ -39,18 +41,6 @@ export const cardHandler = (container, propsArray) => {
         }
     }
     return count;
-};
-export const cardEventHandler = (e) => {
-    const { comp, eventType } = e.detail;
-    const card = comp;
-    const node = card.kulData?.nodes?.[0];
-    switch (eventType) {
-        case 'click':
-            if (node?.value) {
-                window.open(String(node.value).valueOf(), '_blank');
-            }
-            break;
-    }
 };
 export const getCardProps = (container) => {
     const propsArray = [];
@@ -70,5 +60,41 @@ export const getCardProps = (container) => {
 export const createCard = () => {
     const card = document.createElement('kul-card');
     card.addEventListener('kul-card-event', cardEventHandler);
+    card.addEventListener('contextmenu', contextMenuHandler.bind(contextMenuHandler, card));
     return card;
+};
+export const cardEventHandler = (e) => {
+    const { comp, eventType } = e.detail;
+    const card = comp;
+    const node = card.kulData?.nodes?.[0];
+    switch (eventType) {
+        case 'click':
+            if (node?.value) {
+                window.open(String(node.value).valueOf(), '_blank');
+            }
+            break;
+    }
+};
+export const contextMenuHandler = (card, e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const lfManager = getLFManager();
+    const tip = lfManager.getManagers().tooltip;
+    const cb = async (b64image) => {
+        const node = card.kulData?.nodes?.[0];
+        if (node) {
+            const code = node?.cells?.kulCode;
+            if (code) {
+                try {
+                    const path = JSON.parse(code.value).path;
+                    lfManager.log(`Updating cover for model with path: ${path}`, { b64image }, LogSeverity.Info);
+                    getApiRoutes().metadata.updateCover(path, b64image);
+                }
+                catch (error) {
+                    lfManager.log("Failed to fetch the model's path from .info file", { b64image }, LogSeverity.Error);
+                }
+            }
+        }
+    };
+    tip.create({ x: e.x, y: e.y }, 'upload', cb);
 };
